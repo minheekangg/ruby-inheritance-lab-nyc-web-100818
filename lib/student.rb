3e0efac4ae097630be0f7924397abe8390a07ce0
Min Hee  Park <minheekangg@gmail.com>
2018-10-13 23:31:11 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
 
+  attr_accessor :knowledge
+
+    def initialize
+      super
+      @knowledge = []
+    end
+
+  def learn(knowledge)
+    self.knowledge = knowledge
+    @knowledge << knowledge
+  end
 end
